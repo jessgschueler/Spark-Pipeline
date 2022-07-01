3af84130eb088406b45b82723b5c6d2072daf0bc
jessgschueler <jessicagrace.schueler@gmail.com>
2022-07-01 15:09:32 -0700

    WIP commit- write parquet to bq

--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,6 @@
-from operator import truediv
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 
-schema = "Date string, Open float, High float, Low float, Close float, Volume float, Currency string"
+schema = "Date date, Open float, High float, Low float, Close float, Volume float, Currency string"
 df = spark.read.csv('data/coffee.csv', header=True, schema=schema)
